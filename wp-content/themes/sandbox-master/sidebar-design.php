bcd12572e19d8e06989efd9ca2547a648fe2cedf
Paul Thibedeau <pthibedeau@gmail.com>
2014-02-20 20:20:48 -0500

    updates to homepage

---
+++ wp-content/themes/sandbox-master/sidebar-design.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The template for displaying the sidebar.
+ *
+ * @package WordPress
+ * @subpackage Sandbox
+ * @since Sandbox 2.0
+ */
+?>
+
+<aside class="sidebar col span_12">
+
+	<?php //dynamic_sidebar('default'); ?>
+	
+</aside>
