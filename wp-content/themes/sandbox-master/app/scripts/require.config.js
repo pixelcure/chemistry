8157ac10386b7f0e66b8d0917c04fed4df7082dd
Paul Thibedeau <pthibedeau@gmail.com>
2014-03-23 21:03:44 -0400

    cleaning out dup app folders, ran build

--- wp-content/themes/sandbox-master/app/scripts/require.config.js
+++ wp-content/themes/sandbox-master/app/scripts/require.config.js
@@ -34,7 +34,7 @@ require.config({
     },
     paths: {
         underscore : '../bower_components/underscore/underscore-min',
-        jquery: '../bower_components/jquery/jquery-min',
+        jquery: '../bower_components/jquery/jquery.min',
         backbone: '../bower_components/backbone/backbone-min',
         tweenmax: '../bower_components/gsap/src/minified/TweenMax.min',
         scrollmagic: '../bower_components/ScrollMagic/js/jquery.scrollmagic.min',
